bd56d51613dfa975bd21cd209f66a17a9affe38b
Gregor Gorjanc <gregor.gorjanc@roslin.ed.ac.uk>
2016-12-21 12:04:09 +0000

    Added PFUNITDIR and a note on sourcing this file

--- CMakeEddie.sh
+++ CMakeEddie.sh
@@ -1,2 +1,6 @@
+
+# You need to source this file to get variable definitions in your current shell environment, i.e.,
+# source CMakeEddie.sh
 export LD_LIBRARY_PATH=/usr/lib64:$LD_LIBRARY_PATH
 export FC=ifort
+export PFUNITDIR=/exports/cmvm/eddie/eb/groups/hickey_group/pfUnit_serial
