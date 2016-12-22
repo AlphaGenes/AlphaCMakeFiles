8c714dd38a9dfde4fa91e1752cdc386858cbb302
WILSON David <david.wilson@roslin.ed.ac.uk>
2016-12-22 11:03:27 +0000

    fixed doxygen in cmake

--- cmake/modules/findDoxygen.cmake
+++ cmake/modules/findDoxygen.cmake
@@ -1,10 +1,10 @@
 # add a target to generate API documentation with Doxygen
 # find_package(Doxygen)
-# if(DOXYGEN_FOUND)
-# # configure_file(${CMAKE_MODULE_PATH}/../../Doxygen/Doxygen.txt ${BIN}/Doxyfile @ONLY)
-# add_custom_target(doc
-# ${DOXYGEN_EXECUTABLE} ${BIN}/Doxyfile
-# WORKING_DIRECTORY ${BIN}
-# COMMENT "Generating API documentation with Doxygen" VERBATIM
-# )
-# endif(DOXYGEN_FOUND)
+if(DOXYGEN_FOUND)
+configure_file(${CMAKE_MODULE_PATH}/../../Doxygen/Doxygen.txt ${BIN}/Doxyfile @ONLY)
+add_custom_target(doc
+${DOXYGEN_EXECUTABLE} ${BIN}/Doxyfile
+WORKING_DIRECTORY ${BIN}
+COMMENT "Generating API documentation with Doxygen" VERBATIM
+)
+endif(DOXYGEN_FOUND)
