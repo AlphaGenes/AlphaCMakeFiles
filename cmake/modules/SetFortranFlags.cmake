4b550a0f48ac591bd0810eb70fb11245232097b3
David Wilson <david.wilson@roslin.ed.ac.uk>
2017-04-19 14:26:32 +0100

    changed to legacy instructions

--- cmake/modules/SetFortranFlags.cmake
+++ cmake/modules/SetFortranFlags.cmake
@@ -183,8 +183,8 @@ SET_COMPILE_FLAG(CMAKE_Fortran_FLAGS_TESTING "${CMAKE_Fortran_FLAGS_TESTING}"
 #                          "-ta=host"      # Portland Group
 #                 )
 SET_COMPILE_FLAG(CMAKE_Fortran_FLAGS_TESTING "${CMAKE_Fortran_FLAGS_TESTING}"
-    Fortran "-arch SSE4.2"        # Intel
-            "/arch:SSE4.2"       # Intel Windows
+  Fortran "-arch SSSE3"        # Intel
+  "/arch:SSSE3"       # Intel Windows
 
 )
 SET_COMPILE_FLAG(CMAKE_Fortran_FLAGS_TESTING "${CMAKE_Fortran_FLAGS_TESTING}"
